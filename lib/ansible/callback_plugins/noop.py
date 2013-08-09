e93d160fcd788541b58cb563a4f8430cb8efa326
Nicolas Crowell <nicolas.crowell@proteusys.com>
2013-08-09 16:13:09 -0400

    Add task_end playbook callback.

--- lib/ansible/callback_plugins/noop.py
+++ lib/ansible/callback_plugins/noop.py
@@ -72,6 +72,9 @@ class CallbackModule(object):
     def playbook_on_task_start(self, name, is_conditional):
         pass
 
+    def playbook_on_task_end(self, name, results, skipped):
+        pass
+
     def playbook_on_vars_prompt(self, varname, private=True, prompt=None, encrypt=None, confirm=False, salt_size=None, salt=None, default=None):
         pass
 
